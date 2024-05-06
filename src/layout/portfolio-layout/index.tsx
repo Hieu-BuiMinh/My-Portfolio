12775185890e2f175d1f80ba3cd4f68b0d6598b1
BuiMinhHieu <hieu.bm@softworld.jp>
2024-05-06 13:44:20 +0700

    m

--- src/layout/portfolio-layout/index.tsx
+++ src/layout/portfolio-layout/index.tsx
@@ -6,27 +6,34 @@ interface IPortfolioLayout {
 }
 
 function PortfolioLayout({ children }: Readonly<IPortfolioLayout>) {
-	const ogUrl = 'https://hieubm-portfolio.vercel.app/' // Replace with your actual URL
-	const ogTitle = 'Warlock-BM'
-	const ogDescription = 'A concise and compelling description of your page content.'
-	const ogImage = 'https://brittanychiang.com/og.png' // Replace with your image URL
+	const ogData = {
+		title: 'Warlock-BM',
+		description:
+			'Warlock-BM is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+		url: 'https://hieubm-portfolio.vercel.app',
+		image: 'https://brittanychiang.com/og.png',
+		siteName: 'Your Website Name',
+	}
 
 	return (
 		<>
-			<Helmet>
+			<Helmet prioritizeSeoTags>
 				<meta charSet="utf-8" />
 				<title>Warlock-BM</title>
 				<meta
 					name="description"
 					content="Warlock-BM is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences."
 				/>
-				<link rel="apple-touch-icon" href="/assets/images/commons/avatar/avt_01.png" />
-				<link rel="icon" href="/assets/images/commons/avatar/avt_01.png" />
+				<link rel="apple-touch-icon" href="/assets/images/commons/avatar/avt_favicon.png" />
+				<link rel="icon" href="/assets/images/commons/avatar/avt_favicon.png" type="image/png" />
 
-				<meta property="og:url" content={ogUrl} />
-				<meta property="og:title" content={ogTitle} />
-				<meta property="og:description" content={ogDescription} />
-				<meta property="og:image" content={ogImage} />
+				<meta property="og:title" content={ogData.title} />
+				<meta property="og:description" content={ogData.description} />
+				<meta property="og:url" content={ogData.url} />
+				<meta property="og:image" content={ogData.image} />
+				<meta property="og:site_name" content={ogData.siteName} />
+
+				<meta name="description" content={ogData.description} />
 			</Helmet>
 			{children}
 		</>
